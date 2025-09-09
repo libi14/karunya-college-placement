26a0ca424e74efb371c40837d45ae8e56a68ac2e
libi14 <156395257+libi14@users.noreply.github.com>
2025-09-09 22:53:38 +0530

    Create determine the sign.java

---
+++ determine the sign.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n>0)
+            System.out.print("Positive");
+        else if(n<0)
+            System.out.print("Negative");
+        else
+            System.out.print("Zero");
+        sc.close();
+    }
+}
